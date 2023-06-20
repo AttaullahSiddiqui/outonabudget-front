d43b060cea9eb28b6e7265c2f34c3d485d6de792
Attaullah Siddiqui <devmeanstack93@gmail.com>
2023-06-20 18:55:16 +0500

    store page ui done

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DataService } from '../data.service';
+import {
+  faFacebook,
+  faFacebookSquare,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class StoreComponent {}
+export class StoreComponent implements OnInit {
+  faFacebook = faFacebookSquare;
+  faTwitter = faTwitter;
+
+  constructor(private _dataService: DataService) {}
+
+  ngOnInit(): void {}
+}
